7a98c399de5cf8a837527163128a017cd81959a3
BYTTER/chenlw <chenlw@bytter.com>
2020-03-10 11:03:55 +0800

    update

--- src/main/java/crypto/AuthenticationTester.java
+++ src/main/java/crypto/AuthenticationTester.java
@@ -20,6 +20,11 @@ public class AuthenticationTester {
     private static final String KEY = "1234567890abcdef1234567890abcdef";
 
     public static void main(String[] args) {
+        String encryptedBase64String = getEncryptedBase64String(KEY);
+        testDecrypt(KEY,encryptedBase64String);
+    }
+
+    public static void test() {
         UserAuthentication userAuthentication = new UserAuthentication();
         userAuthentication.setUsername("username");
         userAuthentication.setRequestTime(System.currentTimeMillis());
@@ -41,8 +46,52 @@ public class AuthenticationTester {
         } catch (Exception e) {
             System.out.println("异常：" + e.getMessage());
         }
+    }
+
+
+    public static String getEncryptedBase64String(String key) {
+        System.out.println();
+        System.out.println("加密数据...");
+        String result = null;
+        UserAuthentication userAuthentication = new UserAuthentication();
+        userAuthentication.setUsername("username");
+        userAuthentication.setRequestTime(System.currentTimeMillis());
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String json = mapper.writeValueAsString(userAuthentication);
+            System.out.println(json);
+
+            // 256位密钥 = 32 bytes Key:
+            String charsetName = StandardCharsets.UTF_8.name();
+            byte[] keyBytes = key.getBytes(charsetName);
+            // 加密:
+            byte[] data = json.getBytes(charsetName);
+            byte[] encrypted = AES_CBC_Tester.encrypt(keyBytes, data);
+            result = Base64.getEncoder().encodeToString(encrypted);
+            // result = new String(encrypted,charsetName);
+            System.out.println("Encrypted: " + Base64.getEncoder().encodeToString(encrypted));
+            // System.out.println("Encrypted: " + result);
+        } catch (Exception e) {
+            System.out.println("异常：" + e.getMessage());
+        }
+        return result;
+    }
+
+    public static void testDecrypt(String key, String encryptedBase64String) {
+
+        System.out.println();
+        System.out.println("解密数据...");
+        String charsetName = StandardCharsets.UTF_8.name();
+        try {
+            // 解密:
+            byte[] decrypted = AES_CBC_Tester.decrypt(key.getBytes(charsetName),Base64.getDecoder().decode(encryptedBase64String));
+            System.out.println("Decrypted: " + new String(decrypted, charsetName));
+        } catch (Exception e) {
+            System.out.println("解密失败：" + e.getMessage());
+        }
 
     }
+
 
     static class UserAuthentication {
         @JsonProperty("username")
